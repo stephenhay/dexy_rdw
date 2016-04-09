7d09b18760c7264218429884ec432c867222a8b2
Low Kian Seong <lowks@users.noreply.github.com>
2016-04-09 23:26:34 +0800

    Update setup.py
    Adding `description` and `url`

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
         include_package_data = True,
         name='dexy_rdw',
         packages=find_packages(),
+        description='Responsive Design Workflow templates for Dexy',
+        url='https://github.com/stephenhay/dexy_rdw/',
         version="0.0.4",
         install_requires = [
             'dexy>=0.9.8'
